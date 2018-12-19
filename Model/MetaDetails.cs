77c67c9f79b3e101452094c6d96fc0b64611aa0e
darkanegel <45992510+darkanegel@users.noreply.github.com>
2018-12-19 17:16:02 +0800

    Add files via upload

---
+++ Model/MetaDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSEOAnalyser.Model
+{
+    public class MetaDetails
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Words { get; set; }
+        public int TotalWordCount { get; set; }
+        
+    }
+}
